ab48f2171d811298d3d002d8875a0b2675e41868
Matias N Zamora <nikoozamora93@gmail.com>
2022-08-10 20:09:32 -0300

    final

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', ()=> {
     let lon;
     let lat;
+    const API_key = "7723ff91f82f7840f8cf92b3d7072784";
 
     let tempValue = document.getElementById('temp-value');
     let tempDescription = document.getElementById('temp-description');
@@ -11,23 +12,50 @@ window.addEventListener('load', ()=> {
 
     if (navigator.geolocation){
        navigator.geolocation.getCurrentPosition( posicion => {
-        longitude = posicion.coords.longitude
-        latitude = posicion.coords.latitude
-        const url_api = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude},lon=${longitude},appid=7723ff91f82f7840f8cf92b3d7072784`;
-        console.log(url_api)
+        lon = posicion.coords.longitude
+        lat = posicion.coords.latitude
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=${API_key}`;
+        // console.log(url_api)
 
-        // fetch(url)
-        //     .then( response => { return response.json() })
-        //     .then(data => {
-        //         console.log(data)
+        fetch(url)
+            .then( response => { return response.json() })
+            .then(data => {
+                console.log(data.main.temp)
+                let temp = Math.round(data.main.temp)
+                tempValue.textContent = `${temp} °C`
+                tempDescription.textContent = data.weather[0].description
+                locationCity.textContent = data.name
+              
+                switch(data.weather[0].main) {
+                    case 'Clouds':
+                        animateIcon.src = '/animation/cloudy.svg'
+                    break;
+                    case 'Clear':
+                        animateIcon.src = '/animation/day.svg'
+                    break;
+
+                    case 'ThunderStorm':
+                        animateIcon.src = '/animation/thunder.svg'
+                    break;
+                    case 'Drizzle':
+                        animateIcon.src = '/animation/rainy-2.svg'
+                    break;
+                    case 'Snow':
+                        animateIcon.src = '/animation/snowy-6.svg'
+                    break;
+                    case 'Atmosphere':
+                        animateIcon.src = '/animation/weather.svg'
+                    break;
+
+                    default:
+                        animateIcon.src = '/animation/day.svg'
+                    break;
+                }
 
 
-
-        //         .catch(error => {
-        //             console.log(error)
-        //         })
-        //     })
-
+                console.log(data)
+            })
+            .catch(error => { console.log(error) })
        })
     }
 
